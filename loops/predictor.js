ba116221a685d8f253c5cef34ae6bb0d8f9069f7
Isomorphic0 <84076402+Isomorphic0@users.noreply.github.com>
2021-05-25 09:53:11 +0100

    Update predictor.js

--- loops/predictor.js
+++ loops/predictor.js
@@ -874,9 +874,11 @@ const Koviko = {
 
       let totalTime = ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2) + "." + ms;
       let dungeonEquilibrium = Math.sqrt(total / 200000);
-      let dungeonSoulStones = 0
-      if ( !(Number.isNaN(snapshots.currProgress.attributes["Small Dungeon"].value)) ) { dungeonSoulStones += snapshots.currProgress.attributes["Small Dungeon"].value }
-      if ( !(Number.isNaN(snapshots.currProgress.attributes["Large Dungeon"].value)) ) { dungeonSoulStones += snapshots.currProgress.attributes["Large Dungeon"].value * 10 }
+      let dungeonSoulStones = 0;
+      if ( !(Number.isNaN(snapshots.currProgress.attributes["Small Dungeon"].value)) ) { dungeonSoulStones += snapshots.currProgress.attributes["Small Dungeon"].value };
+	    console.log(Number.isNaN(snapshots.currProgress.attributes["Small Dungeon"].value));
+      if ( !(Number.isNaN(snapshots.currProgress.attributes["Large Dungeon"].value)) ) { dungeonSoulStones += snapshots.currProgress.attributes["Large Dungeon"].value * 10 };
+	    console.log(Number.isNaN(snapshots.currProgress.attributes["Small Dungeon"].value));
       // let dungeonSoulStones = snapshots.currProgress.attributes["Small Dungeon"].value + snapshots.currProgress.attributes["Large Dungeon"].value * 10;
       let expectedSoulStones = dungeonEquilibrium * dungeonSoulStones + this.predictions["Mine Soulstones"].loopsCompleted;
       let soulStonesPerMinute = expectedSoulStones / totalTicks * 60;
