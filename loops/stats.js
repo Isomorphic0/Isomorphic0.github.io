1d2a91abbec77edf5c757c75bfa066ca913950df
Isomorphic0 <84076402+Isomorphic0@users.noreply.github.com>
2021-05-26 14:58:35 +0100

    Update stats.js

--- loops/stats.js
+++ loops/stats.js
@@ -101,7 +101,7 @@ function getSelfCombat() {
 }
 
 function getTeamCombat() {
-    return getSelfCombat("Combat") + getSkillLevel("Restoration") * 2 + resources.zombies * 0.5 * getSkillLevel("Dark Magic") + (getSkillLevel("Combat") + getSkillLevel("Restoration") * 2) * (resources.teamMembers / 2) * getAdvGuildRank().bonus;
+    return getSelfCombat("Combat") + getSkillLevel("Restoration") * 2 + resources.zombies * 0.5 * getSkillLevel("Dark") + (getSkillLevel("Combat") + getSkillLevel("Restoration") * 2) * (resources.teamMembers / 2) * getAdvGuildRank().bonus;
 }
 
 function getPrcToNextSkillLevel(skill) {
